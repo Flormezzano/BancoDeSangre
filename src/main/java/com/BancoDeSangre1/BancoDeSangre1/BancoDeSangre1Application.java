9f0b2b512ad620f8745f1b9fe8402519dc7d7db3
GastonA99 <soycanalla_gaston@hotmail.com>
2021-10-26 19:23:17 -0300

    Inicio

---
+++ src/main/java/com/BancoDeSangre1/BancoDeSangre1/BancoDeSangre1Application.java
@@ -0,0 +1,13 @@
+package com.BancoDeSangre1.BancoDeSangre1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDeSangre1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDeSangre1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/BancoDeSangre1/BancoDeSangre1/BancoDeSangre1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BancoDeSangre1.BancoDeSangre1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDeSangre1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
