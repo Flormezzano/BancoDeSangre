876bb844d85733c0a7f4536d0575e29ef416d716
GastonA99 <soycanalla_gaston@hotmail.com>
2021-10-27 20:31:17 -0300

    Exception

---
+++ src/main/java/com/BancoDeSangre1/BancoDeSangre1/exception/ExceptionServicio.java
@@ -0,0 +1,12 @@
+package com.BancoDeSangre1.BancoDeSangre1.exception;
+
+/**
+ *
+ * @author Gastón
+ */
+public class ExceptionServicio extends Exception{
+
+    public ExceptionServicio(String msj){
+        super(msj);
+    }
+}
