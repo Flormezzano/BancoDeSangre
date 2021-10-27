b8ea67ec12c41f98561010d5ae7d35cbaedb8255
Flormezzano <mezzanoflorencia0@gmail.com>
2021-10-27 20:05:33 -0300

    cambios provincia

---
+++ src/main/java/com/BancoDeSangre1/BancoDeSangre1/Servicios/ProvinciaService.java
@@ -0,0 +1,23 @@
+
+package com.BancoDeSangre1.BancoDeSangre1.Servicios;
+
+import com.BancoDeSangre1.BancoDeSangre1.Repositorios.ProvinciaRepo;
+import com.BancoDeSangre1.BancoDeSangre1.entidades.Provincia;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ProvinciaService {
+    
+    @Autowired
+    ProvinciaRepo PciaRepo; 
+    
+    public List<Provincia> findByName(String name){
+        List<Provincia>provincias=PciaRepo.findByName(name);
+        return provincias;
+    }
+    
+    public List<Provincia> listar(){
+     List<Provincia> provincias = PciaRepo.findAll();
+      return provincias;
+}
+}
