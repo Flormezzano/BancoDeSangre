326a1cefc395583a288f40fa1df998ad6d937663
Flormezzano <mezzanoflorencia0@gmail.com>
2021-10-27 21:03:25 -0300

    cambios

--- src/main/java/com/BancoDeSangre1/BancoDeSangre1/Repositorios/ProvinciaRepo.java
+++ src/main/java/com/BancoDeSangre1/BancoDeSangre1/Repositorios/ProvinciaRepo.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProvinciaRepo extends JpaRepository<Provincia, String> {
     
     @Query("SELECT c FROM Provincia c WHERE c.nombre LIKE :name")
--- src/main/java/com/BancoDeSangre1/BancoDeSangre1/Servicios/ProvinciaService.java
+++ src/main/java/com/BancoDeSangre1/BancoDeSangre1/Servicios/ProvinciaService.java
@@ -5,7 +5,9 @@ import com.BancoDeSangre1.BancoDeSangre1.Repositorios.ProvinciaRepo;
 import com.BancoDeSangre1.BancoDeSangre1.entidades.Provincia;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProvinciaService {
     
     @Autowired
