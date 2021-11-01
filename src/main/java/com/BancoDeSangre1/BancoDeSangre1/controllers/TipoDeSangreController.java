76e1da6d168831ce7c8d5ca4e15d8fa13a1a5919
Martin <martinefraile@gmail.com>
2021-11-01 14:30:18 -0300

    ServCtrlRepo TipoSangre

---
+++ src/main/java/com/BancoDeSangre1/BancoDeSangre1/controllers/TipoDeSangreController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.BancoDeSangre1.BancoDeSangre1.controllers;
+
+import com.BancoDeSangre1.BancoDeSangre1.Servicios.TipoDeSangreService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Martin Fraile
+ */
+@Controller
+@RequestMapping("")
+public class TipoDeSangreController {
+    TipoDeSangreService TipoDeSangreServicio;
+}
