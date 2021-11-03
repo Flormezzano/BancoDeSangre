772676a6958fd43ccba94379c2151b87d56436b4
Flormezzano <mezzanoflorencia0@gmail.com>
2021-11-02 21:34:20 -0300

    cambios de package

--- src/main/java/com/BancoDeSangre1/BancoDeSangre1/controllers/CiudadController.java
+++ src/main/java/com/BancoDeSangre1/BancoDeSangre1/controllers/CiudadController.java
@@ -18,18 +18,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/ciudad")
 public class CiudadController {
 
-//    @Autowired
-//    private CiudadService ciudadServicio;
-//    
-//    @GetMapping("/lista")
-//    public String lista(Model model, @RequestParam(required = false) String nombre){
-//        if (nombre != null) {
-//            List<Ciudad> provincia = ciudadServicio.listaProvinciaPorNombre(nombre);
-//            model.addAttribute("provincia", provincia);
-//        } else {
-//            List<Ciudad> provincia = ciudadServicio.listaCiudad();
-//            model.addAttribute("provincia", provincia);
-//        }
-//        return "indexC";
-//    }
+    @Autowired
+    private CiudadService ciudadServicio;
+    
+    @GetMapping("/lista")
+    public String lista(Model model, @RequestParam(required = false) String nombre){
+        if (nombre != null) {
+            List<Ciudad> provincia = ciudadServicio.listaProvinciaPorNombre(nombre);
+            model.addAttribute("provincia", provincia);
+        } else {
+            List<Ciudad> provincia = ciudadServicio.listaCiudad();
+            model.addAttribute("provincia", provincia);
+        }
+        return "indexC";
+    }
 }
