5c96563b382d68513b719b7b2888fba077ec3e7b
GastonA99 <soycanalla_gaston@hotmail.com>
2021-11-10 17:37:02 -0300

    Registro

--- src/main/java/com/BancoDeSangre1/BancoDeSangre1/controllers/LoginControlador.java
+++ src/main/java/com/BancoDeSangre1/BancoDeSangre1/controllers/LoginControlador.java
@@ -6,17 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ *
+ * @author Gastón
+ */
+
 @Controller
 @RequestMapping("/login")
-public class loginController {
-    
-    
+public class LoginControlador {
+
     @GetMapping("")
-    public String login(Model model, @RequestParam(required = false) String error,@RequestParam(required = false) String mail,@RequestParam(required = false) String logout){
-        if (error!=null) {
+    public String login(Model model, @RequestParam(required = false) String error, @RequestParam(required = false) String mail, @RequestParam(required = false) String logout){
+        if (error != null) {
             model.addAttribute("error", "El usuario ingresado o la contraseña son incorrectas");
         }
-        if (mail!=null) {
+        if (mail != null) {
             model.addAttribute("mail", mail);
         }
         return "index";
--- src/main/java/com/BancoDeSangre1/BancoDeSangre1/controllers/controllerGral.java
+++ src/main/java/com/BancoDeSangre1/BancoDeSangre1/controllers/controllerGral.java
@@ -1,5 +1,7 @@
 package com.BancoDeSangre1.BancoDeSangre1.controllers;
 
+import com.BancoDeSangre1.BancoDeSangre1.entidades.Persona;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +10,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class controllerGral {
    
-//    @GetMapping("/nosotros")
-//    public String nosotros(){
-//        return "nosotros";
-//    }
-//    
-//    @GetMapping("/error")
-//    public String error(){
-//        return "error";
-//    }
+    @GetMapping("/nosotros")
+    public String nosotros(){
+        return "nosotros";
+    }
+    
+    @GetMapping("/error")
+    public String error(){
+        return "error";
+    }
 
+    @GetMapping("/inicioUsuario")
+    public String inicioUsuario(){
+        return "inicioUsuario";
+    }
+    
+    @GetMapping("")
+    public String index(HttpSession session) throws Exception{
+        Persona persona = (Persona) session.getAttribute("persona");
+        try{
+            if(persona != null){
+                switch(persona.getRol()){
+                    case USER:
+                        return "redirect:/inicioUsuario";
+                    case ADMIN:
+                        return "redirect:/";
+                }
+            }
+        }catch(Exception e){
+            throw new Exception("Lo siento");
+        }
+        return "index";
+    }
 }
